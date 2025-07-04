5b1b50eee7561565903d55a61aa234e1461fa260
Elton N. <16689434+eltonnuness@users.noreply.github.com>
2025-07-04 15:00:38 -0300

    Pixelation Render Feature
    - Added pixelation render feature

---
+++ Assets/Scripts/Pixelation/PixelationRenderFeature.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Rendering.RenderGraphModule.Util;
+using UnityEngine.Rendering.Universal;
+
+namespace PSX
+{
+    public class PixelationRenderFeature : ScriptableRendererFeature
+    {
+        PixelationPass pixelationPass;
+
+        public override void Create()
+        {
+            pixelationPass = new PixelationPass(RenderPassEvent.BeforeRenderingPostProcessing);
+        }
+
+        //ScripstableRendererFeature is an abstract class, you need this method
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            renderer.EnqueuePass(pixelationPass);
+        }
+    }
+    
+    
+    public class PixelationPass : ScriptableRenderPass
+    {
+        class PassData
+        {
+            internal TextureHandle textureToRead;
+        }
+
+        private static readonly string shaderPath = "PostEffect/Pixelation";
+        
+        //PROPERTIES
+        static readonly int WidthPixelation = Shader.PropertyToID("_WidthPixelation");
+        static readonly int HeightPixelation = Shader.PropertyToID("_HeightPixelation");
+        static readonly int ColorPrecison = Shader.PropertyToID("_ColorPrecision");
+
+        Pixelation pixelation;
+        Material pixelationMaterial;
+
+        public PixelationPass(RenderPassEvent evt)
+        {
+            renderPassEvent = evt;
+            var shader = Shader.Find(shaderPath);
+            if (shader == null)
+            {
+                Debug.LogError("Shader not found.");
+                return;
+            }
+            this.pixelationMaterial = new Material(shader);
+        }
+
+        public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+        {
+            if (this.pixelationMaterial == null)
+            {
+                Debug.LogError("Material not created.");
+                return;
+            }
+
+            UniversalCameraData cameraData = frameData.Get<UniversalCameraData>();
+            if (!cameraData.postProcessEnabled) return;
+
+            UniversalResourceData resourceData = frameData.Get<UniversalResourceData>();
+            TextureHandle srcCamColor = resourceData.cameraColor;
+            Debug.Log($"descriptorName: {srcCamColor.GetDescriptor(renderGraph).name}");
+
+            var stack = VolumeManager.instance.stack;
+
+            this.pixelation = stack.GetComponent<Pixelation>();
+            if (this.pixelation == null) { return; }
+            if (!this.pixelation.IsActive()) { return; }
+
+            // Render (start)
+            Debug.Log("Start render");
+            var source = srcCamColor;
+
+            //getting camera width and height 
+            var w = cameraData.camera.scaledPixelWidth;
+            var h = cameraData.camera.scaledPixelHeight;
+
+            //setting parameters here 
+            cameraData.camera.depthTextureMode = cameraData.camera.depthTextureMode | DepthTextureMode.Depth;
+            this.pixelationMaterial.SetFloat(WidthPixelation, this.pixelation.widthPixelation.value);
+            this.pixelationMaterial.SetFloat(HeightPixelation, this.pixelation.heightPixelation.value);
+            this.pixelationMaterial.SetFloat(ColorPrecison, this.pixelation.colorPrecision.value);
+
+            TextureDesc pixelationTextureDescriptor = srcCamColor.GetDescriptor(renderGraph);
+            pixelationTextureDescriptor.name = "_TempPixelation";
+            pixelationTextureDescriptor.depthBufferBits = 0;
+            pixelationTextureDescriptor.msaaSamples = MSAASamples.None;
+            pixelationTextureDescriptor.filterMode = FilterMode.Point;
+            var dst = renderGraph.CreateTexture(pixelationTextureDescriptor);
+
+            if (!srcCamColor.IsValid() || !dst.IsValid())
+            {
+                Debug.Log($"srcCamColor: {srcCamColor.IsValid()} | dst: {dst.IsValid()}");
+                return;
+            }
+
+            RenderGraphUtils.BlitMaterialParameters simplePass = new(source, dst, Blitter.GetBlitMaterial(TextureDimension.Tex2D), 0);
+            renderGraph.AddBlitPass(simplePass, "Source -> TempTexture");
+
+            RenderGraphUtils.BlitMaterialParameters pixelationPass = new(dst, source, pixelationMaterial, 0);
+            renderGraph.AddBlitPass(pixelationPass, "TempTexture -> Pixelation -> Source");
+
+        }
+
+    }
+}
---
+++ Assets/Shaders/Pixelation.shader
@@ -0,0 +1,47 @@
+﻿Shader "PostEffect/Pixelation"
+{
+    HLSLINCLUDE
+        #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+        #include "Packages/com.unity.render-pipelines.core/Runtime/Utilities/Blit.hlsl"
+
+        // for Pixelation
+        float _WidthPixelation;
+        float _HeightPixelation;
+        
+        // for color precision
+        float _ColorPrecision;
+
+        float4 Frag(Varyings input) : SV_Target
+        {
+            //UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
+
+            // Pixelation
+            float2 uv = input.texcoord;
+            uv.x = floor(uv.x * _WidthPixelation) / _WidthPixelation;
+            uv.y = floor(uv.y * _HeightPixelation) / _HeightPixelation;
+            
+            float4 color = SAMPLE_TEXTURE2D(_BlitTexture, sampler_LinearClamp, uv);
+
+            // Color precision
+            color = floor(color * _ColorPrecision) / _ColorPrecision;
+            return color;
+        }
+    ENDHLSL
+
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" "RenderPipeline" = "UniversalPipeline"}
+        LOD 100
+        ZWrite Off Cull Off
+
+        Pass
+        {
+            Name "PixelationPass"
+
+            HLSLPROGRAM
+            #pragma vertex Vert
+            #pragma fragment Frag
+            ENDHLSL
+        }
+    }
+}
